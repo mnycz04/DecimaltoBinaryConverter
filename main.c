eb60ed9116def032598118cc4ab040c4270ed195
Michael Nycz <mnycz04@gmail.com>
2022-10-16 11:42:50 -0400

    Retyped, and added comments

--- main.c
+++ main.c
@@ -6,64 +6,94 @@
 #define TRUE 1
 #define FALSE 0
 
+typedef uint8_t bool;
+
+char *binary_array_to_cstring_64(uint8_t *array) {
+    char *string = malloc(sizeof(char) * 80);
+    uint8_t digit_counter = 0;
+
+    *(string + 79) = '\0'; // terminates C-string
+
+    for (int i = 0, j = 0; i < 64; i++, j++) {
+        if (digit_counter == 4) {
+            digit_counter = 0;
+            *(string + j) = ' ';
+            j++;
+        }
+        *(string + j) = *(array + i) + 48; // 48 is ASCII for 0;
+        digit_counter++;
+    }
+
+    return string;
+}
+
 int main(void) {
     int64_t *number = malloc(sizeof(int64_t));
-    unsigned *binary_string = malloc(sizeof(unsigned) * 64);
-    unsigned *hex_string = malloc(sizeof(unsigned) * 16);
-    unsigned int negative_flag = FALSE;
+    uint8_t *binary_string = malloc(sizeof(uint8_t) * 64);
+    uint8_t *hex_string = malloc(sizeof(uint8_t) * 16);
+    bool negative_flag = FALSE;
 
-    printf("Please enter a valid 64-Bit signed integer: ");
-    scanf("%li", number);
+    printf("Enter a valid, 64-bit signed decimal integer: ");
+    scanf("%lli", number);
+    printf("\n");
 
-    if (*number < 0) { // Gets Absolute Value if the number is negative
+    // Check for Negative Value, if it is, gets the absolute value and stores it in *number
+    if (*number < 0ll) {
         negative_flag = TRUE;
-        *number = ~*number; // Flips the bits
-        (*number)++;
+        *number = ~*number; // Flips All bits in *number
+        (*number)++; // Adds one to get the actual absolute value
     }
 
-    for (int i = 63; i >= 0; i--) { // Traverses the array in reverse
-        *(binary_string + i) = *number % 2;
+    // Binary Section
+
+    // Traverses the empty (uninitialized) array that'll hold the binary values, in reverse
+    // By repeatedly dividing the original value by 2, the remainder gives us the next binary value,
+    // starting from the least significant bit (LSB) all the way until the most.
+    for (uint8_t *bit = binary_string + 63; bit >= binary_string; bit--) {
+        *bit = *number % 2;
         *number /= 2;
     }
+    free(number);
+
+    printf("Unsigned Binary Value:\n\t%s\n", binary_array_to_cstring_64(binary_string));
 
     if (negative_flag) {
         for (int i = 63; i >= 0; i--) {
+            // Finds the first least significant 1 in the values binary
             if (*(binary_string + i) == 1) {
-                for (int j = i - 1; j >= 0; j--) {
-                    *(binary_string + j) ^= 1; // Flips Bits
+                // Iterates over all remaining bits to the left of the bit found above
+                for (uint8_t *bit = binary_string + i - 1; bit >= binary_string; bit--) {
+                    *bit ^= 1; // Bitwise XOR against 1 flips every bit
                 }
-                break;
+                break; // Stops after executing once
             }
         }
     }
 
-    printf("Binary Value:\n\t");
+    printf("Signed Binary Value:\n\t%s\n", binary_array_to_cstring_64(binary_string));
 
-    unsigned digit_counter = 0; // Used to track which bit is being printed to IO
-    for (unsigned *i = binary_string; i - binary_string < 64; i++) {
-        // Prints the Binary value, skipping a space every 4 bits
-        printf("%u", *i);
-        if (digit_counter == 3) {
-            printf(" ");
-            digit_counter = 0;
-            continue;
-        }
-        digit_counter++;
-    }
 
-    for (int i = 0; i < 16; i++) { // Splits the binary array into 16 groups, each representing 4 bits
-        *(hex_string + i) = 0;
+    // Hex Section
+
+    // Groups the binary values into groups of 4 that are converted into their Hex representations
+    for (int i = 0; i < 16; i++) {
+        *(hex_string + i) = 0; // Initializes all values to be 0b00000000
         for (int j = 0; j < 4; j++) {
-            *(hex_string + i) <<= 1; // Shifts the bit of the previous value to the left 1
-            *(hex_string + i) |= *(binary_string + ((4 * i) + j)); // Concatenates the next bit
+            // Shifts the current value to the left once, freeing the LSB for the next bit value
+            *(hex_string + i) <<= 1;
+
+            // 'Appends' the next bit to the end of the above value
+            *(hex_string + i) |= *(binary_string + (4 * i) + j);
         }
     }
 
-    printf("\nHex Value:\n\t0x");
-
-    for (int i = 0; i < 16; i++) { // Prints the Hex Values
+    printf("Hexadecimal Value:\n\t0x");
+    for (int i = 0; i < 16; i++) {
         printf("%x", *(hex_string + i));
     }
+
+    free(binary_string);
+    free(hex_string);
 
     return 0;
 }
